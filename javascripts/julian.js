e593129954926e7394cd4f18a0583027234d8b21
Brian Gerhards <briangerhards@gmail.com>
2016-06-15 22:51:13 -0500

    Added additional row and column highlighting to better help navigate to day and month. Added ability to select and unselect day. Highlights day and month along with row and column to help line up page.

--- javascripts/julian.js
+++ javascripts/julian.js
@@ -8,20 +8,33 @@ $(function() {
     // Set number of days for Leap Year
     numDays[1] = currentYear % 4 == 0 ? 29 : 28;
     startDay = getVarStartDay(numDays);
-    for (var i = 0; i < 12; i++) {
-        console.log(calendarMonths[i] + ": " + numDays[i]);
-    }
+
+    // Create the table
     addTable();
+
+    // Add calendar months to the header
     addHeaders(calendarMonths);
+
+    // Add table body
     addRows(numDays, startDay);
 
     $('td').mouseover(function() {
         var col = $(this).parent().children().index($(this));
         var row = $(this).parent().parent().children().index($(this).parent());
         $(".highlightedMonDay").removeClass("highlightedMonDay");
-        $('.julian  thead tr:eq(0) th:eq(' + col + ')').addClass("highlightedMonDay");
-        $('.julian  tbody tr:eq(' + row + ') th:eq(0)').addClass("highlightedMonDay");
-        // alert('Row: ' + row + ', Column: ' + col);
+        $(getHighlightRowCol(row, col)).addClass("highlightedMonDay");
+    });
+
+    $('td').click(function() {
+        if ($(this).hasClass("selectedMonDay")) {
+            $(".selectedMonDay").removeClass("selectedMonDay");
+        } else {
+            var col = $(this).parent().children().index($(this));
+            var row = $(this).parent().parent().children().index($(this).parent());
+            $(".selectedMonDay").removeClass("selectedMonDay");
+            $(getHighlightRowCol(row, col)).addClass("selectedMonDay");
+            $(this).addClass("selectedMonDay");
+        }
     });
 });
 
@@ -29,33 +42,28 @@ function addTable() {
     $('.julian').append('<table class="table table-striped table-bordered"><thead></thead><tbody></tbody></table>');
 }
 
+function getHighlightRowCol(row, col) {
+    return '.julian  thead tr:eq(0) th:eq(' + col + ') ,.julian  tbody tr:eq(' + row + ') th, .julian tbody tr td:nth-child(' + (col + 1) + ') ,.julian  tbody tr:eq(' + row + ') td'
+}
+
 function getVarStartDay(numDays) {
     var startDay = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
     for (var i = 0; i < 12; i++) {
-        for (var a = i; a > 0; a--) {
-            startDay[i] += numDays[a - 1];
-        }
+        for (var a = i; a > 0; a--) startDay[i] += numDays[a - 1];
     }
     return startDay;
 }
 
 function addHeaders(calendarMonths) {
-    $('.julian  thead').append('<tr><th class="julianDayCount"></th>' +
-        "</tr>");
-    for (var i = 0; i < 12; i++) {
-        $('.julian thead tr:last').append('<th class="julianHeader">' + calendarMonths[i] + '</th>');
-    }
-
+    $('.julian  thead').append('<tr><th class="julianDayCount"></th></tr>');
+    for (var i = 0; i < 12; i++) $('.julian thead tr:last').append('<th class="julianHeader">' + calendarMonths[i] + '</th>');
+    $('.julian thead tr:last').append('<th class="julianDayCount"></th>')
 }
 
 function addRows(numDays, startDay) {
     for (var i = 0; i < 31; i++) {
         $('.julian  tbody').append('<tr><th class="julianDayCount">' + (i + 1) + '</td>' + "</tr>");
-
-        for (var a = 0; a < 12; a++) {
-            $('.julian  tbody tr:last').append('<td class="julianBody">' + (numDays[a] > i ? startDay[a] + i : '') + '</td>');
-        }
+        for (var a = 0; a < 12; a++) $('.julian  tbody tr:last').append('<td class="julianBody">' + (numDays[a] > i ? startDay[a] + i : '') + '</td>');
+        $('.julian  tbody tr:last').append('<th class="julianDayCount">' + (i + 1) + '</th>');
     }
-
-
 }
